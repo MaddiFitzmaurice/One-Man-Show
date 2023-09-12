6724c390c65b52d33fbc10acc9edb4a376d2889b
Aidan Davies <poppers@davies.net.au>
2023-09-12 11:26:31 +1000

    Changed EnemyManager pool creation
    EnemyManager now stores a list of EnemyPrefabType
    This makes it so new enemy types can be added/removed in the inspector, and the EnemyManager will dynamically create a pool for each

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -2,38 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Stores the prefab and its associated parent
+[System.Serializable]
+public struct EnemyPrefabType
+{
+    public GameObject prefab;
+    public Transform parent;
+}
+
 public class EnemyManager : MonoBehaviour
 {
-    [SerializeField] List<EnemySpawner> _enemySpawns;
+    //[SerializeField] List<EnemySpawner> _enemySpawns;
 
     // Enemy prefabs to pool
-    [SerializeField] GameObject _type1EnemyPrefab;
-    [SerializeField] GameObject _type2EnemyPrefab;
-    [SerializeField] GameObject _type3EnemyPrefab;
+    [SerializeField] List<EnemyPrefabType> _enemyPrefabs;
 
     // Maximum number of enemies per pool
     [SerializeField] int _maxNumToPool;
 
-    // Parents that enemies are parented to so the hierarchy is clean and organised
-    [SerializeField] Transform _type1EnemyParent;
-    [SerializeField] Transform _type2EnemyParent;
-    [SerializeField] Transform _type3EnemyParent;
-
-    // Enemy pool lists
-    List<GameObject> _type1EnemyList;
-    List<GameObject> _type2EnemyList;
-    List<GameObject> _type3EnemyList;
+    // Enemy pool lists for all prefabs
+    List<List<GameObject>> _typeEnemyList;
 
     private void Start()
     {
+        _typeEnemyList = new List<List<GameObject>>();
         InitEnemyPools();
     }
 
     // Create enemy pools
     void InitEnemyPools()
     {
-        _type1EnemyList = ObjectPooler.CreateObjectPool(_maxNumToPool, _type1EnemyPrefab, _type1EnemyParent);
-        _type2EnemyList = ObjectPooler.CreateObjectPool(_maxNumToPool, _type2EnemyPrefab, _type2EnemyParent);
-        _type3EnemyList = ObjectPooler.CreateObjectPool(_maxNumToPool, _type3EnemyPrefab, _type3EnemyParent);
+        foreach (EnemyPrefabType ept in _enemyPrefabs)
+		{
+            _typeEnemyList.Add(ObjectPooler.CreateObjectPool(_maxNumToPool, ept.prefab, ept.parent));
+		}
     }
 }
